88c05babfef68bb4e74ff1e2271524dfe81db8c1
Mogtaba-Alim <alim.mogtaba@gmail.com>
2022-02-19 11:13:32 -0500

    feat: initalize project

---
+++ src/emotionData/emotion.java
@@ -0,0 +1,13 @@
+package emotionData;
+
+public class emotion {
+    private final String name;
+    private final String likeliness;
+
+
+    public emotion(String name, String likeliness) {
+        this.name = name;
+        this.likeliness = likeliness;
+    }
+}
+
---
+++ src/imageIn/image.java
@@ -0,0 +1,9 @@
+package imageIn;
+
+public class image {
+    private final String image_address;
+
+    public image(String image_address) {
+        this.image_address = image_address;
+    }
+}
