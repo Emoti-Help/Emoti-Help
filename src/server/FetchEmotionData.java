06bbdea9fa0596eaa6e50dbfa4d85db42a61e5b2
Mogtaba-Alim <alim.mogtaba@gmail.com>
2022-02-20 07:39:36 -0500

    feat: fixed some issues

--- src/server/FetchEmotionData.java
+++ src/server/FetchEmotionData.java
@@ -1,18 +1,16 @@
 package server;
 
-import com.google.cloud.vision.v1.AnnotateImageRequest;
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
-import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.*;
 import com.google.cloud.vision.v1.Feature.Type;
-import com.google.cloud.vision.v1.Image;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.protobuf.ByteString;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,7 +18,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import emotionParser.emotionParser;
-import user.currentUser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import user.user;
 
 public class FetchEmotionData {
     public static void main(String... args) throws Exception {
@@ -30,9 +30,16 @@ public class FetchEmotionData {
         // The path to the image file to annotate
         String fileName = "../Images/sad_face.jpg";
         try {
-            JsonObject jsonResponse = labelImage(fileName);
-            currentUser user = emotionParser.readData(jsonResponse);
-            System.out.println(user);
+            // Creates a FileOutputStream
+            FileOutputStream file = new FileOutputStream("../Images/test.txt");
+
+            // Creates a PrintStream
+            PrintStream output = new PrintStream(file, true);
+            detectText(fileName, output);
+//            JSONObject jsonResponse = labelImage(fileName);
+//            user user = emotionParser.readData(jsonResponse);
+//            System.out.println(user);
+
 
 
         } catch (IOException e) {
@@ -41,7 +48,40 @@ public class FetchEmotionData {
 
     }
 
-    public static JsonObject labelImage(String fileName) throws IOException {
+    public static void detectText(String filePath, PrintStream out) throws Exception, IOException {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+                    out.printf("Text: %s\n", annotation.getDescription());
+                    out.printf("Position : %s\n", annotation.getBoundingPoly());
+                }
+            }
+        }
+    }
+
+    public static JSONObject labelImage(String fileName) throws IOException {
+
+
+
 
         // Reads the image file into memory
         Path path = Paths.get(fileName);
@@ -53,7 +93,7 @@ public class FetchEmotionData {
             // Builds the image annotation request
             List<AnnotateImageRequest> requests = new ArrayList<>();
             Image img = Image.newBuilder().setContent(imgBytes).build();
-            Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+            Feature feat = Feature.newBuilder().setType(Type.FACE_DETECTION).build();
             AnnotateImageRequest request =
                     AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
             requests.add(request);
@@ -62,8 +102,8 @@ public class FetchEmotionData {
             BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
             List<AnnotateImageResponse> responses = response.getResponsesList();
             String JsonString = new Gson().toJson(response);
-            JsonParser parser = new JsonParser();
-            JsonObject finalResponse = (JsonObject) parser.parse(JsonString);
+            Object obj = JSONValue.parse(JsonString);
+            JSONObject finalResponse = (JSONObject) obj;
             return finalResponse;
         }
 
